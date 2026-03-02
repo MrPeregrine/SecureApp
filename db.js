18fa21ec72561c24571d79fc8b1161f7ba015deb
MrPeregrine <92043960+MrPeregrine@users.noreply.github.com>
2026-03-01 21:22:02 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: parseInt(process.env.MYSQLPORT) || 3306,
+  uri: process.env.MYSQL_PUBLIC_URL, // URL pública de Railway
   waitForConnections: true,
   connectionLimit: 10
 });
+
+module.exports = pool;
 
 // Reintentos de conexión
 (async () => {
